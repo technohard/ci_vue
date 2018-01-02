ebebc71f07d762bf7732195e8ff35fa72cba3e67
technohard <maizier.b@gmail.com>
2018-01-02 16:58:16 +0700

    add more files

---
+++ application/modules/welcome/views/welcomevue.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Modern Business - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo site_url() ?>public/assets/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div id="app"></div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo site_url() ?>public/assets/template/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo site_url() ?>public/assets/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo site_url() ?>src/dist/build.js"></script>
+    
+
+
+  </body>
+
+</html>
