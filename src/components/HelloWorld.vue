ed6839a10d03549d95d99f715a158b5db0af667e
technohard <maizier.b@gmail.com>
2018-01-02 17:19:13 +0700

    update repo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Hello World Welcome to Your Vue.js App '
+      msg: 'Welcome to Your Vue.js App build with Codeigniter 3.1.6 '
     }
   }
 }
